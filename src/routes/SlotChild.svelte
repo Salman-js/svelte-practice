5c3f9cb07ed91f7434043b6d80e3cb885acb78c3
Salman M <salmanmoh.eth@gmail.com>
2023-03-09 19:34:15 +0300

    slot props

--- src/routes/SlotChild.svelte
+++ src/routes/SlotChild.svelte
@@ -1,8 +1,9 @@
 <script>
+  const fname = 'Salman';
 </script>
 
 <main>
   <slot>Default slot content</slot>
-  <slot name="first-named">Default 1 slot content</slot>
+  <slot name="first-named" {fname}>Default 1 slot content</slot>
   <slot name="second-named">Default 2 slot content</slot>
 </main>
--- src/routes/SlotParent.svelte
+++ src/routes/SlotParent.svelte
@@ -7,8 +7,8 @@
     <h2>This is a slot from the parent component</h2>
   </SlotChild>
   <SlotChild>
-    <h2 slot="first-named">
-      This is the first named slot from the parent component
+    <h2 slot="first-named" let:fname>
+      This is the first named slot from the parent component with prop {fname}
     </h2>
   </SlotChild>
   <SlotChild>
