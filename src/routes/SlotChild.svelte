f593f108c7436f08f98d1e43aed1e050ee1dd6ea
Salman M <salmanmoh.eth@gmail.com>
2023-03-09 19:29:28 +0300

    named slots

--- src/routes/SlotChild.svelte
+++ src/routes/SlotChild.svelte
@@ -3,4 +3,6 @@
 
 <main>
   <slot>Default slot content</slot>
+  <slot name="first-named">Default 1 slot content</slot>
+  <slot name="second-named">Default 2 slot content</slot>
 </main>
--- src/routes/SlotParent.svelte
+++ src/routes/SlotParent.svelte
@@ -6,5 +6,15 @@
   <SlotChild>
     <h2>This is a slot from the parent component</h2>
   </SlotChild>
+  <SlotChild>
+    <h2 slot="first-named">
+      This is the first named slot from the parent component
+    </h2>
+  </SlotChild>
+  <SlotChild>
+    <h2 slot="second-named">
+      This is the second slot from the parent component
+    </h2>
+  </SlotChild>
   <SlotChild />
 </main>
